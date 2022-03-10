3d41f4cba6c67869b1808dc29c8006043669ac13
karokaromm <101354540+karokaromm@users.noreply.github.com>
2022-03-10 21:02:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە lera Link Dane`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/Z2Gty4UgBU`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("id Channel ").send(
+  client.channels.get("720714019270557738").send(
 `> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`RaMa is here`) 
+client.user.setGame(`k4ro is here`) 
 });
 
 
 
 
-client.login("");// Token Lera Dane
+client.login("OTQwNjg4MDIxNDEyNTM2MzYw.YgLB_Q.SYBftzxyp8N6tjil0awAtO3mtSM");// Token Lera Dane
